168c22c87ac3885b3dfcd502bc290756302620fa
Gajan Nagaraj <thegajannagaraj@gmail.com>
2020-04-24 18:12:13 -0700

    asset issuing working

--- main.go
+++ main.go
@@ -28,7 +28,13 @@ type Payment struct {
   DEST    string  `json:"destination" binding:"required"`
   Amount  string  `json:"amount" binding:"required"`
   Asset   string  `json:"asset" binding:"required"`
-  Issuer  string  `json:"issuer" binding:"required"`
+  Issuer  string  `json:"issuer"`
+}
+
+type Asset struct {
+  SK      string  `json:"key" binding:"required"`
+  Asset   string  `json:"asset" binding:"required"`
+  Amount   string  `json:"amount" binding:"required"`
 }
 
 //******************************************************************************
@@ -44,17 +50,25 @@ type KeyPair struct {
   SK      string    `json:"private_key"`
 }
 
+type NewAsset struct {
+  Asset         string  `json:"asset" binding:"required"`
+  Amount        string  `json:"amount" binding:"required"`
+  Issuer        KeyPair `json:"issuer" binding:"required"`
+  Distribution  KeyPair `json:"distribution" binding:"required"`
+}
+
 func main() {
   r := gin.Default()
   api :=  r.Group("/dex/api/v1")
   {
     api.GET("/", index)
     //ACCOUNT ROUTES
-    api.GET("/newAccount", newAccount)
+    api.GET("/createAccount", createAccount)
     api.POST("/viewAccount", viewAccount)
     //PAYMENT ROUTES
     api.POST("/payment", payment)
-    // api.DELETE("/deleteAccount", deleteAccount)
+    //ASSET ROUTES
+    api.POST("/createAsset", createAsset)
   }
   r.Run()
 }
@@ -66,7 +80,7 @@ func index(c *gin.Context) {
 //TODO: validate account exists
 //      abstract root account code into separate function
 //      root account has enough funds
-func newAccount(c *gin.Context) {
+func createAccount(c *gin.Context) {
   kp, err := keypair.Random()
   if err != nil {
     log.Fatal(err)
@@ -81,14 +95,15 @@ func newAccount(c *gin.Context) {
     ar := horizonclient.AccountRequest{AccountID: master_kp.Address()}
     sourceAccount, err := CLIENT.AccountDetail(ar)
     if err != nil {
-      error(c, http.StatusInternalServerError, "Failed to fund account.")
+      error(c, http.StatusInternalServerError, "Failed to create account.")
       log.Fatal(err)
     }
+
   //******************************************************************************
 
     createAccountOp := txnbuild.CreateAccount{
         Destination: kp.Address(),
-        Amount:      "10",
+        Amount:      "100",
     }
 
     tx := txnbuild.Transaction{
@@ -102,9 +117,9 @@ func newAccount(c *gin.Context) {
 
     _, err = CLIENT.SubmitTransactionXDR(txeBase64)
     if err != nil {
-        hError := err.(*horizonclient.Error)
-        error(c, http.StatusInternalServerError, "Failed to fund account.")
-        log.Fatal("Error submitting transaction: ", hError)
+      error(c, http.StatusInternalServerError, "Failed to fund account.")
+      hError := err.(*horizonclient.Error)
+      log.Fatal("Error submitting transaction: ", hError)
     }
 
   d := KeyPair{
@@ -150,16 +165,25 @@ func payment(c *gin.Context) {
     log.Fatal(err)
   }
 
-  assetRequest := horizonclient.AssetRequest{ForAssetCode: asset, ForAssetIssuer: issuer}
-  hAsset0, err := CLIENT.Assets(assetRequest)
-  log.Println(hAsset0)
+  var a txnbuild.Asset
 
-  a := txnbuild.CreditAsset{Code: asset, Issuer: issuer}
+  if asset != "XLM" {
+    assetRequest := horizonclient.AssetRequest{ForAssetCode: asset, ForAssetIssuer: issuer}
+    hAsset0, err := CLIENT.Assets(assetRequest)
+    if err != nil {
+      error(c, http.StatusInternalServerError, "Payment failed.")
+      log.Fatal(err)
+    }
+    log.Println(hAsset0)
+    a = txnbuild.CreditAsset{Code: asset, Issuer: issuer}
+  } else {
+    a = txnbuild.NativeAsset{}
+  }
 
   op := txnbuild.Payment{
     Destination:    destination,
     Amount:         amount,
-    Asset:          &a,
+    Asset:          a,
   }
 
   tx := txnbuild.Transaction{
@@ -186,6 +210,159 @@ func payment(c *gin.Context) {
 
   response(c, http.StatusOK, "Payment successful.", nil)
   }
+
+//TODO: abstract account creation to a separate fucntion
+//       abstract account details to a separate function
+func createAsset(c *gin.Context) {
+  var a Asset
+  c.BindJSON(&a)
+  source := a.SK
+  asset := a.Asset
+  amount := a.Amount
+
+  //get source account details
+  source_kp, _ := keypair.Parse(source)
+  ar := horizonclient.AccountRequest{AccountID: source_kp.Address()}
+  sourceAccount, err := CLIENT.AccountDetail(ar)
+  if err != nil {
+    error(c, http.StatusInternalServerError, "Failed to create asset.")
+    log.Fatal(err)
+  }
+
+  //create issuing account
+  issuer_kp, err := keypair.Random()
+  if err != nil {
+    log.Fatal(err)
+    error(c, http.StatusInternalServerError, "Failed to create asset.")
+  }
+
+  createAccountOp := txnbuild.CreateAccount{
+      Destination: issuer_kp.Address(),
+      Amount:      "100",
+  }
+
+  tx := txnbuild.Transaction{
+      SourceAccount: &sourceAccount,
+      Operations:    []txnbuild.Operation{&createAccountOp},
+      Timebounds:    txnbuild.NewTimeout(300),
+      Network:       "Standalone Network ; February 2017",
+  }
+
+  txeBase64, err := tx.BuildSignEncode(source_kp.(*keypair.Full))
+
+  _, err = CLIENT.SubmitTransactionXDR(txeBase64)
+  if err != nil {
+      hError := err.(*horizonclient.Error)
+      error(c, http.StatusInternalServerError, "Failed to create asset.")
+      log.Fatal("Error submitting transaction: ", hError)
+  }
+
+  //distribution issuing account
+  distribution_kp, err := keypair.Random()
+  if err != nil {
+    log.Fatal(err)
+    error(c, http.StatusInternalServerError, "Failed to create asset.")
+  }
+
+  createAccountOp = txnbuild.CreateAccount{
+      Destination: distribution_kp.Address(),
+      Amount:      "100",
+  }
+
+  tx = txnbuild.Transaction{
+      SourceAccount: &sourceAccount,
+      Operations:    []txnbuild.Operation{&createAccountOp},
+      Timebounds:    txnbuild.NewTimeout(300),
+      Network:       "Standalone Network ; February 2017",
+  }
+
+  txeBase64, err = tx.BuildSignEncode(source_kp.(*keypair.Full))
+
+  _, err = CLIENT.SubmitTransactionXDR(txeBase64)
+  if err != nil {
+      hError := err.(*horizonclient.Error)
+      error(c, http.StatusInternalServerError, "Failed to create asset.")
+      log.Fatal("Error submitting transaction: ", hError)
+  }
+
+  //create asset and establish trust
+  ir := horizonclient.AccountRequest{AccountID: issuer_kp.Address()}
+  issueAccount, err := CLIENT.AccountDetail(ir)
+  if err != nil {
+    error(c, http.StatusInternalServerError, "Failed to create asset.")
+    log.Fatal(err)
+  }
+  dr := horizonclient.AccountRequest{AccountID: distribution_kp.Address()}
+  distAccount, err := CLIENT.AccountDetail(dr)
+  if err != nil {
+    error(c, http.StatusInternalServerError, "Failed to create asset.")
+    log.Fatal(err)
+  }
+
+  ca := txnbuild.CreditAsset{Code: asset, Issuer: issuer_kp.Address()}
+
+  changeTrustOp := txnbuild.ChangeTrust{
+    Line:           &ca,
+  }
+
+  tx = txnbuild.Transaction{
+      SourceAccount: &distAccount,
+      Operations:    []txnbuild.Operation{&changeTrustOp},
+      Timebounds:    txnbuild.NewTimeout(300),
+      Network:       "Standalone Network ; February 2017",
+  }
+
+  // dist_kp_parse, _ := keypair.Parse(distribution_kp)
+  txe, err := tx.BuildSignEncode(distribution_kp)
+  if err != nil {
+    hError := err.(*horizonclient.Error)
+    error(c, http.StatusInternalServerError, "Payment failed.")
+    log.Fatal("Error submitting transaction: ", hError)
+  }
+  _, err = CLIENT.SubmitTransactionXDR(txe)
+  if err != nil {
+      hError := err.(*horizonclient.Error)
+      error(c, http.StatusInternalServerError, "Payment failed.")
+      log.Fatal("Error submitting transaction: ", hError)
+  }
+
+  log.Println("trust established")
+
+  op := txnbuild.Payment{
+    Destination:    distribution_kp.Address(),
+    Amount:         amount,
+    Asset:          &ca,
+  }
+
+  tx = txnbuild.Transaction{
+      SourceAccount: &issueAccount,
+      Operations:    []txnbuild.Operation{&op},
+      Timebounds:    txnbuild.NewTimeout(300),
+      Network:       "Standalone Network ; February 2017",
+  }
+
+  txe, err = tx.BuildSignEncode(issuer_kp)
+  if err != nil {
+    hError := err.(*horizonclient.Error)
+    error(c, http.StatusInternalServerError, "Payment failed.")
+    log.Fatal("Error submitting transaction: ", hError)
+  }
+  _, err = CLIENT.SubmitTransactionXDR(txe)
+  if err != nil {
+      hError := err.(*horizonclient.Error)
+      error(c, http.StatusInternalServerError, "Payment failed.")
+      log.Fatal("Error submitting transaction: ", hError)
+  }
+
+  na := NewAsset{
+    Asset: asset,
+    Amount: amount,
+    Issuer: KeyPair{PK: issuer_kp.Address(), SK: issuer_kp.Seed()},
+    Distribution: KeyPair{PK: distribution_kp.Address(), SK: distribution_kp.Seed()},
+  }
+
+  response(c, http.StatusOK, asset + " created and " + amount + " issued.", &na)
+}
 
 //TODO: place this error function into a separate utils file
 func error(c *gin.Context, status int, message string) {
